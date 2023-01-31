d000db093d7e31a1607d7ad18067aedae0ced890
Sudeep MP <sudipmelepunathilmp@gmail.com>
2023-01-31 18:56:42 +0000

    basic list component created with icon support

--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
 import { rgba } from 'polished'
+import Icon from '@mdi/react';
+import { mdiHome } from '@mdi/js';
+
 
 const ListStyled = styled.ul`
     padding: 0;
 `;
 
 const ListItemStyled = styled.li`
-    background-color: ;
+    background-color: transparent;
     padding: ${props => props.theme.spacing.md}px;
     display: flex;
     align-items: center;
@@ -15,14 +18,31 @@ const ListItemStyled = styled.li`
     &:hover {
       background-color: ${({ theme }) => theme.ListItem.background};
     }
+    svg {
+      margin-right: 8px;
+    }
 `;
+
 
 const List = ({ children }) => {
   return <ListStyled>{children}</ListStyled>;
 };
 
-const ListItem = ({ children }) => {
-  return <ListItemStyled>{children}</ListItemStyled>;
+const ListItem = ({ children, leftIcon, rightIcon, isActive, iconSize=1  }) => {
+  return (
+    <ListItemStyled isActive={isActive}>
+      {leftIcon && <Icon path={leftIcon} size={iconSize} />}
+      {children}
+      {rightIcon && <Icon path={rightIcon} size={iconSize} />}
+    </ListItemStyled>
+  );
+};
+
+ListItem.defaultProps = {
+  leftIcon: mdiHome,
+  rightIcon: null,
+  isActive: false,
+  iconSize: 1,
 };
 
 export { List, ListItem };
