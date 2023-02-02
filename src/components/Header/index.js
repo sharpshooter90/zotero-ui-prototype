38e701baed5cc8bfcef7b608f8bc6affcb57803d
Sudeep MP <sudipmelepunathilmp@gmail.com>
2023-02-02 22:07:28 +0000

    updated project name added storybook and created story for component Button

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,25 +1,20 @@
 import React from "react";
 import styled from "styled-components";
 import Button from "../Button";
-import Icon from '@mdi/react';
-import { mdiPlus } from '@mdi/js';
-import { mdiCloudSync } from '@mdi/js';
-
-
-
+import { mdiPlus, mdiCloudSync } from "@mdi/js";
 
 const StyledHeader = styled.header`
-display: flex;
-flex-direction: row;
-justify-content: space-between;
-align-items: center;
-padding: 10px;
-gap: 10px;
-height: 46px;
+  display: flex;
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  padding: 10px;
+  gap: 10px;
+  height: 46px;
 
-background-color: ${props => props.theme.headerBackgroundColor};
-color: ${props => props.theme.headerTextColor};
-border-bottom: 1px solid #343434;
+  background-color: ${(props) => props.theme.headerBackgroundColor};
+  color: ${(props) => props.theme.headerTextColor};
+  border-bottom: 1px solid #343434;
 `;
 
 const StyledLeftCol = styled.div`
@@ -33,14 +28,22 @@ const StyledRightCol = styled.div`
   gap: 10px;
 `;
 
-
 export default function Header() {
   return (
     <StyledHeader>
       <StyledLeftCol></StyledLeftCol>
       <StyledRightCol>
-        <Button variant="secondary" size="xs" leftIcon={mdiCloudSync} iconSize={0.8}>Sync</Button>
-        <Button variant="primary" size="xs" leftIcon={mdiPlus} iconSize={0.8}>Add New</Button>
+        <Button
+          variant="secondary"
+          size="xs"
+          leftIcon={mdiCloudSync}
+          iconSize={0.8}
+        >
+          Sync
+        </Button>
+        <Button variant="primary" size="xs" leftIcon={mdiPlus} iconSize={0.8}>
+          Add New
+        </Button>
       </StyledRightCol>
     </StyledHeader>
   );
