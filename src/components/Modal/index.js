d011166faad643c467066ff703a4883ea6fb3474
Sudeep MP <sudipmelepunathilmp@gmail.com>
2023-02-06 18:29:48 +0000

    modal base styling separate components for header, footer, content

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -7,23 +7,29 @@ const ModalOverlay = styled.div`
   bottom: 0;
   left: 0;
   right: 0;
-  background-color: rgba(0, 0, 0, 0.5);
+  background-color: rgba(0, 0, 0, 0.75);
   display: flex;
   align-items: center;
   justify-content: center;
   z-index: 1;
 `;
 
-const ModalContent = styled.div`
-  background-color: white;
-  border-radius: 4px;
-  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
-  max-height: 80%;
-  max-width: 600px;
-  overflow-y: auto;
-  padding: 20px;
+const StyledModalContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: flex-start;
+  padding: 0px;
+  gap: 10px;
+
   position: relative;
-  color: #000;
+
+  overflow-y: scroll;
+
+  background: #151515;
+  border-radius: 12px;
+  max-height: 80%;
+  max-width: 800px;
+  width: 800px;
 `;
 
 const CloseButton = styled.button`
@@ -36,15 +42,44 @@ const CloseButton = styled.button`
   right: 0px;
 `;
 
+const StyledModalHeader = styled.div`
+  background: #111111;
+  padding: 24px;
+  width: 100%;
+`;
+
+const StyledModalFooter = styled.div`
+  padding: 1rem;
+  display: flex;
+  justify-content: flex-end;
+`;
+
+const StyledModalContent = styled.div`
+  padding: 0px 24px;
+`;
+
+const ModalHeader = ({ children }) => (
+  <StyledModalHeader>{children}</StyledModalHeader>
+);
+
+const ModalFooter = ({ children }) => (
+  <StyledModalFooter>{children}</StyledModalFooter>
+);
+
+const ModalContent = ({ children }) => (
+  <StyledModalContent>{children}</StyledModalContent>
+);
+
 const Modal = ({ isOpen, children, onClose }) => {
   return isOpen ? (
     <ModalOverlay>
-      <ModalContent>
+      <StyledModalContainer>
         <CloseButton onClick={onClose}>Close</CloseButton>
         {children}
-      </ModalContent>
+      </StyledModalContainer>
     </ModalOverlay>
   ) : null;
 };
 
+export { ModalHeader, ModalFooter, ModalContent };
 export default Modal;
