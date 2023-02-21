d1c447bacb55c4d39fd2ed70dd1082cb52e0f067
Sudeep MP <sudipmelepunathilmp@gmail.com>
2023-02-21 16:48:45 +0000

    created Tag component added tags and search to search modal

---
+++ src/components/Tag/index.js
@@ -0,0 +1,79 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+
+import Icon from "@mdi/react";
+
+const sizes = {
+  small: {
+    fontSize: "12px",
+    padding: "4px 12px",
+  },
+  medium: {
+    fontSize: "14px",
+    padding: "6px 12px",
+  },
+  large: {
+    fontSize: "16px",
+    padding: "8px 16px",
+  },
+};
+
+const TagWrapper = styled.div`
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  font-size: ${(props) => sizes[props.size].fontSize};
+  padding: ${(props) => sizes[props.size].padding};
+  border-radius: 24px;
+  border: 1px solid ${(props) => props.theme.colors[props.theme.mode].border};
+  cursor: pointer;
+
+  &:hover {
+    background-color: ${(props) => props.theme.colors.backgroundHover};
+  }
+
+  ${(props) =>
+    props.active &&
+    `
+    background-color: ${props.theme.colors.gray};
+    color: ${props.theme.colors[props.theme.mode].text};
+    border-color: ${props.theme.colors[props.theme.mode].border};
+  `}
+`;
+
+const LeftIconWrapper = styled.span`
+  margin-right: 4px;
+  display: inline-flex;
+`;
+
+const RightIconWrapper = styled.span`
+  margin-left: 4px;
+  display: inline-flex;
+`;
+
+const Tag = ({ title, size = "medium", leftIcon, rightIcon }) => {
+  const [active, setActive] = useState(false);
+
+  const handleClick = () => {
+    setActive(!active);
+  };
+
+  return (
+    <TagWrapper size={size} active={active} onClick={handleClick}>
+      {leftIcon && (
+        <LeftIconWrapper>
+          {" "}
+          <Icon path={leftIcon} size={0.6} />
+        </LeftIconWrapper>
+      )}
+      {title}
+      {rightIcon && (
+        <RightIconWrapper>
+          <Icon path={rightIcon} size={0.6} />
+        </RightIconWrapper>
+      )}
+    </TagWrapper>
+  );
+};
+
+export default Tag;
